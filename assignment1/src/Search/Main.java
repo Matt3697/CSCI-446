1a8b2c29d25eab6a6d9f096b8331c08ba33b0697
Matthew Sagen <matthewsagen@matthews-mbp.home>
2018-10-02 15:09:47 -0600

    added some output functionality

--- assignment1/src/Search/Main.java
+++ assignment1/src/Search/Main.java
@@ -1,20 +1,27 @@
 package Search;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		Maze mediumMaze = new Maze("medium_maze");
 		Maze largeMaze = new Maze("large_maze");
 		Maze openMaze = new Maze("open_maze");
 		mediumMaze.printMaze();
 		largeMaze.printMaze();
 		openMaze.printMaze();
-		
+		PrintWriter writer = new PrintWriter("Output.txt", "UTF-8"); //outputs to directory Assignment1
+		printResults(writer);
+		writer.close();
 	}
-	
+	public static void printResults(PrintWriter writer) {
+		writer.print("Some results...");
+	}
 	
 }
