b18eb57378d1f9c47a7e3007f51c90f3a1cca44a
Matthew Sagen <matthewsagen@matthews-mbp.home>
2018-10-02 14:58:42 -0600

    argument parsing

--- assignment1/src/Search/Main.java
+++ assignment1/src/Search/Main.java
@@ -7,39 +7,14 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		create_mazes("medium_maze");
-		create_mazes("large_maze");
-		create_mazes("open_maze");
+		Maze mediumMaze = new Maze("medium_maze");
+		Maze largeMaze = new Maze("large_maze");
+		Maze openMaze = new Maze("open_maze");
+		mediumMaze.printMaze();
+		largeMaze.printMaze();
+		openMaze.printMaze();
 		
 	}
 	
-	public static void create_mazes(String mazeType) { //read in the mazes into double arrays.
-		Scanner mazeScanner = null;
-		char[][] maze = null;
-		if(mazeType == "medium") {
-			maze = new char[62][22];
-		}
-		else if(mazeType == "large") {
-			maze = new char[81][30];
-		}
-		else if(mazeType == "open") {
-			maze = new char[37][19];
-		}
-		
-		//Opens file location
-        try{
-        		mazeScanner = new Scanner(new File("Search/" + mazeType + "/.txt"));
-        }
-        //throws error if the file can't be located.
-        catch(Exception e){
-            System.out.println("File not found.");
-            System.exit(0);
-        }
-        //add data to maze list.
-        for(int i = 0; i < maze[0].length; i++) {
-        		for(int y = 0; y < maze.length; y++) {
-        			maze[i][y] = (char) mazeScanner.nextByte();
-        		}
-        }
-   }
+	
 }
--- assignment1/src/Search/Maze.java
+++ assignment1/src/Search/Maze.java
@@ -1,9 +1,70 @@
 package Search;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Maze {
-	public Maze(ArrayList<String> maze) {
+	public String mazeType;
+	public char[][] maze = null;
+	
+	public Maze(String mazeType) {
+		this.mazeType = mazeType;
+		create_maze(mazeType);
+	}
+	
+	public void create_maze(String mazeType) { //read in a maze into "maze".
+		Scanner mazeScanner = null;
+		String[] rows = null;
+		char[] columns = null;
+		if(mazeType == "medium_maze") {
+			maze = new char[23][62];//[rows][columns]
+			rows = new String[23];
+			columns = new char[62];
+			
+		}
+		else if(mazeType == "large_maze") {
+			maze = new char[31][81];//[rows][columns]
+			rows = new String[31];
+			columns = new char[81];
+		}
+		else if(mazeType == "open_maze") {
+			maze = new char[20][37];//[rows][columns]
+			rows = new String[20];
+			columns = new char[37];
+		}
 		
+		//Opens file location
+        try{
+        		mazeScanner = new Scanner(new File("src/Search/" + mazeType + ".txt"));
+        }
+        //throws error if the file can't be located.
+        catch(Exception e){
+            System.out.println("File not found.");
+            System.exit(0);
+        }
+        //add data to maze list.
+        System.out.println(maze.length + ":" + maze[0].length);
+        for(int i = 0; i < rows.length; i++) {
+        		rows[i] = mazeScanner.nextLine();
+        }
+        for(int i = 0; i < rows.length; i++) {//loop through each row and add each index as an index of a column
+        		columns = rows[i].toCharArray();
+			for(int y = 0; y < columns.length; y++) {
+				maze[i][y] = columns[y];
+			}
+        }
+       
+      
+   }
+	public void printMaze() {
+		 for(int i = 0; i < maze.length; i++) {
+     		for(int y = 0; y < maze[0].length; y++) {
+     			System.out.print(maze[i][y]);;
+     		}
+     		System.out.println();
+     }
+		 System.out.println();
+		 System.out.println();
 	}
 }
