f35d2a9e8aa0aa9bc0184e784be2a4fd351dd215
Matthew Sagen <matthewsagen@matthews-mbp.home>
2018-10-03 21:57:55 -0600

    More work on DFS

--- assignment1/src/Search/DFS.java
+++ assignment1/src/Search/DFS.java
@@ -1,45 +1,131 @@
 package Search;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Stack;
 
 /*
  * DFS (Depth First Search)
  */
 public class DFS {
-	int startingX;
-	int startingY;
-	HashMap<Integer, Integer> xy = new HashMap<Integer,Integer>();
+	public int startingX;
+	public int startingY;
+	public HashMap<Integer, Integer> path = new HashMap<Integer,Integer>();
+	public Frontier frontier = new Frontier();
+	public char[][] matrix = null;
 	
 	public DFS() {
 		
 	}
 	
 	public void solve(Maze maze) {
-		char[][] matrix = maze.getMatrix();
-		computeStartingPoint(matrix);
-		for(int i = startingY; i < matrix.length; i++) {		  //looping through columns
-			for(int y = startingX; y < matrix[0].length; i++) {//looping through rows
-				if(startingX == 1 && startingY == 1) {		  //top left corner
-					break;
-				}
-				else if(startingX == 1 && startingY == matrix.length - 2) { //bottom left corner
-					System.out.println("HI");
-					break;
-				}
-				else if(startingX == matrix.length - 1 && startingY == matrix[0].length - 2) { // bottom right corner
-					break;
-				}
-				else if(startingX == matrix.length - 2 && startingY == 1) { //top right corner
-					break;
-				}
-				else {
-					break;
-				}
+		
+		matrix = maze.getMatrix();
+		getInitialState(matrix); //step 1: get initial state.
+		int y = startingY;
+		int x = startingX;
+		frontier.push(x,y); //push initial state onto frontier. path is empty.
+		path.put(x,y);      //add to output sequence
+		matrix[x][y] = '.';  //mark the node as visited
+		
+		int[] xy = new int [2];
+		boolean goal = false;
+		int i = 0;
+		while(!goal) {
+			if(path.isEmpty()) { //Return fail if the frontier is null.
+				System.out.println("FAIL.");
+				System.exit(0);
 			}
+			if(!visited(x+1,y)) { //if we have not expanded the node to the right.
+				if(matrix[x+1][y] == '%') { //if there are no children nodes, then pop the node from the stack.
+					frontier.pop();
+					break;
+				}
+				else{
+					frontier.push(x+1,y);
+				}
+				path.put(x+1,y);
+				x += 1;
+			}
+			else if(!visited(x-1,y)) { //if we have not expanded the node to the left.
+				if(matrix[x-1][y] == '%') { //if there are no children nodes, then pop the node from the stack.
+					frontier.pop();
+					break;
+				}
+				else{
+					frontier.push(x-1,y);
+				}
+				path.put(x-1, y);
+				x -= 1;
+			}
+			else if(!visited(x,y+1)) {//if we have not expanded the node below.
+				if(matrix[x][y+1] == '%') { //if there are no children nodes, then pop the node from the stack.
+					frontier.pop();
+					break;
+				}
+				else{
+					frontier.push(x,y+1);
+				}
+				path.put(x, y+1);
+				y += 1;
+			}
+			else if(!visited(x,y-1)) {//if we have not expanded the node above.
+				if(matrix[x][y-1] == '%') { //if there are no children nodes, then pop the node from the stack.
+					frontier.pop();
+					break;
+				}
+				else{
+					frontier.push(x,y-1);
+				}
+				path.put(x, y-1);
+				y -= 1;
+			}
+			matrix[x][y] = '.'; //mark the node as visited.
+			goal = checkForGoalState(x,y); //Step 4: Check for goal state
 		}
+		printStack();
 	}
 	
-	public void computeStartingPoint(char[][] matrix) { //helper method to get starting point.
+	public boolean visited(int x, int y) {
+		if(path.containsKey(x) && path.get(x) != y) { //return false if we have not visited a spot.
+			return false;
+		}
+		else { //return true if we have visited the spot
+			return true;
+		}
+	}
+	public boolean checkForGoalState(int x, int y) {
+		boolean goal = false;
+		if(matrix[x-1][y] == '*'){//if the goal is to the left
+			goal = true;
+			frontier.push(x-1, y);
+			path.put(x-1, y); //add the x and y position to the path list
+		}
+		else if(matrix[x][y+1] == '*'){//if the goal is above
+			goal = true;
+			frontier.push(x, y+1);
+			path.put(x, y+1); //add the x and y position to the path list
+		}
+		else if(matrix[x+1][y] == '*'){//if the goal is to the right
+			goal = true;
+			frontier.push(x+1, y);
+			path.put(x+1, y); //add the x and y position to the path list
+		}
+		else if(matrix[x][y-1] == '*'){//if the goal is below
+			goal = true;
+			frontier.push(x, y-1);
+			path.put(x, y-1); //add the x and y position to the path list
+		}
+		return goal;
+	}
+	public void printStack() {
+		ArrayList<Integer> x = frontier.getStackX();
+		ArrayList<Integer> y = frontier.getStackY();
+		for(int i = 0; i < x.size(); i++) {
+			System.out.println(x.get(i) + "," + y.get(i));
+		}
+	}
+	public void getInitialState(char[][] matrix) { //helper method to get the inital state.
 		for(int i = 0; i < matrix.length; i++) { //looping through columns
 			for(int y = 0; y < matrix[0].length; y++) { //looping through rows
      			if(matrix[i][y] == 'P') {
---
+++ assignment1/src/Search/Frontier.java
@@ -0,0 +1,37 @@
+package Search;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Frontier {
+	public Stack<Integer> xLoc = new Stack<Integer>();
+	public Stack<Integer> yLoc = new Stack<Integer>();
+	
+	public Frontier() {
+		
+	}
+	
+	public void push(int x, int y) {
+		xLoc.push(x);
+		yLoc.push(y);
+	}
+	public void pop() {
+		xLoc.pop();
+		yLoc.pop();
+	}
+	public ArrayList<Integer> getStackX() {
+		ArrayList<Integer> x = new ArrayList<Integer>();
+		while(!xLoc.isEmpty()) {
+			x.add(xLoc.pop());
+		}
+		return x;
+	}
+	public ArrayList<Integer> getStackY() {
+		ArrayList<Integer> y = new ArrayList<Integer>();
+		while(!xLoc.isEmpty()) {
+			y.add(yLoc.pop());
+		}
+		return y;
+	}
+	
+}
