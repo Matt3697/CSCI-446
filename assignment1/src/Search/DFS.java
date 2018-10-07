992b99bfdafdd08269ef2e547e26f0d2828b8d1b
Matthew Sagen <matthewsagen@matthews-mbp.home>
2018-10-07 15:51:08 -0600

    changes

--- assignment1/src/Search/DFS.java
+++ assignment1/src/Search/DFS.java
@@ -11,6 +11,7 @@ public class DFS {
 	public int startingX;
 	public int startingY;
 	public HashMap<Integer, Integer> path = new HashMap<Integer,Integer>();
+	public Stack<Node> nodes = new Stack<Node>();
 	public Frontier frontier = new Frontier();
 	public char[][] matrix = null;
 	
@@ -19,15 +20,150 @@ public class DFS {
 	}
 	
 	public void solve(Maze maze) {
-		
 		matrix = maze.getMatrix();
 		getInitialState(matrix); //step 1: get initial state.
 		int y = startingY; 
 		int x = startingX;
-		Tree tree = new Tree(x,y,matrix);
-		tree.createTree();
+		//Step One: push starting node to the stack,update the path, and mark it as visited.
+		boolean goal = false;
+		Node root = new Node(x,y);
+		root.setParent(null);
+		nodes.push(root);
+		path.put(y, x);
+		Node node = null;
+		Node lastnode = root;
+		matrix[y][x] = '.';
+		int i = 0;
+		//Step Two: while node has left child, and the goal is not met, follow left branch.
+		//Once there is no left child,find closest parent with a right child. follow its right branch.
+		while(!goal) {
+			System.out.println(i);
+			printMaze();
+			int numberSurroundings = 0;
+			if(!visited(y, x+1)) { //if we have not visited the space to the right.
+				x += 1;
+				node = new Node(x,y);
+				path.put(y,x);
+				node.setParent(lastnode);
+				numberSurroundings = checkSurroundings(node);
+				System.out.println("num:" + numberSurroundings);
+				if(matrix[y][x+1] == '%'){
+						handleWall(node);
+				}
+			}
+			else if(!visited(y-1, x)) {//if we have not visited the space above.
+				y -= 1;
+				node = new Node(x,y);
+				path.put(y,x);
+				node.setParent(lastnode);
+				numberSurroundings = checkSurroundings(node);
+				System.out.println("num:" + numberSurroundings);
+				if(matrix[y-1][x] == '%'){
+					handleWall(node);
+				}
+			}
+			else if(!visited(y, x-1)) { //if we have not visited the space to the left.
+				x -= 1;
+				node = new Node(x,y);
+				path.put(y,x);
+				node.setParent(lastnode);
+				numberSurroundings = checkSurroundings(node);
+				System.out.println("num:" + numberSurroundings);
+				if(matrix[y][x-1] == '%'){
+					handleWall(node);
+				}
+			}
+			else if(!visited(y+1,x)) {//if we have not visited the space below.
+				y += 1;
+				node = new Node(x,y);
+				path.put(y,x);
+				node.setParent(lastnode);
+				numberSurroundings = checkSurroundings(node);
+				System.out.println("num:" + numberSurroundings);
+				if(matrix[y+1][x] == '%'){
+					handleWall(node);
+				}
+			}
+			if(numberSurroundings > 1) { //if there are alternate paths at a node, set alternatePaths to true.
+				node.setAlternatePath(true);
+			}
+			if(matrix[y][x] == ' ') {
+				lastnode.setLeftChild(node);	
+				matrix[y][x] = '.';
+			}
+			goal = checkForGoalState(y,x); //Check for goal state
+			lastnode = node;
+			i++;
+		}
 	}
-	
+	public Node getNextPath(Node node) {//find the closest parent node with another possible path.
+		while(!node.getParent().hasAlternatePath()) {
+			System.out.println(node.getX());
+			node = node.getParent();
+		}
+		return node;
+	}
+	public void handleWall(Node node) {
+		Node ancestorNode = getNextPath(node);
+		int lastX = ancestorNode.getX();
+		int lastY = ancestorNode.getY();
+		int x = ancestorNode.getPossiblePathX();
+		int y = ancestorNode.getPossiblePathY();
+		node = new Node(x,y);
+		node.setParent(ancestorNode);
+		ancestorNode.setRightChild(node);
+	}
+	public int checkSurroundings(Node node) {//checks to see if there are any alternate paths at this node.
+		int counter = 0;
+		int y = node.getY();
+		int x = node.getX();
+		if(matrix[y][x+1] == ' ' && !visited(y,x+1)) {
+			counter++;
+			node.addPossiblePathX(x+1); 
+			node.addPossiblePathY(y);
+		}
+		if(matrix[y][x-1] == ' ' && !visited(y,x-1)) {
+			counter++;
+			node.addPossiblePathX(x-1); 
+			node.addPossiblePathY(y);		}
+		if(matrix[y+1][x] == ' ' && !visited(y+1,x)) {
+			counter++;
+			node.addPossiblePathX(x); 
+			node.addPossiblePathY(y+1);		}
+		if(matrix[y-1][x] == ' ' && !visited(y-1,x)) {
+			counter++;
+			node.addPossiblePathX(x); 
+			node.addPossiblePathY(y-1);		}
+		return counter;
+	}
+	public boolean checkForGoalState(int y, int x) {
+		boolean goal = false;
+		if(matrix[y][x] == '*'){//if the goal is to the left
+			goal = true;
+			path.put(x, y); //add the x and y position to the path list
+		}
+		else if(matrix[y][x] == '*'){//if the goal is above
+			goal = true;
+			path.put(x, y); //add the x and y position to the path list
+		}
+		else if(matrix[y][x] == '*'){//if the goal is to the right
+			goal = true;
+			path.put(y,x); //add the x and y position to the path list
+		}
+		else if(matrix[y][x] == '*'){//if the goal is below
+			goal = true;
+			path.put(y,x); //add the x and y position to the path list
+		}
+		return goal;
+	}
+	public boolean visited(int y, int x) {
+		if(path.containsKey(y) && path.get(y) != x) { //return false if we have not visited a spot.
+			return false;
+		}
+		else { //return true if we have visited the spot
+			return true;
+		}
+	}
 	public void printStack() {
 		for(int i = 0; i < matrix.length; i++) { //looping through columns
 			for(int y = 0; y < matrix[0].length; y++) { //looping through rows
@@ -50,6 +186,14 @@ public class DFS {
      				break;
      			}
      		}
+		}
+	}
+	public void printMaze() {
+		for(int i = 0; i < matrix.length; i++) { //looping through columns
+			for(int y = 0; y < matrix[0].length; y++) { //looping through rows
+     			System.out.print(matrix[i][y]);
+     		}
+			System.out.println();
 		}
 	}
 }
--- assignment1/src/Search/Node.java
+++ assignment1/src/Search/Node.java
@@ -1,11 +1,19 @@
 package Search;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Node {
 	Node right;
 	Node left;
 	Node parent;
 	boolean leftChild,rightChild,goal;
+	boolean alternatePath = false;
 	int x,y;
+	ArrayList<Integer> possiblePathX = new ArrayList<Integer>();
+	ArrayList<Integer> possiblePathY = new ArrayList<Integer>();
+
+	
 	public Node(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -33,6 +41,42 @@ public class Node {
 		else {
 			return false;
 		}
+	}
+	public void addPossiblePathX(int x) {
+		possiblePathX.add(x);
+	}
+	public void addPossiblePathY(int Y) {
+		possiblePathY.add(y);
+	}
+	public int getPossiblePathX(){
+		int x = -1;
+		//if(possiblePathX.size() > 0) {
+			x = possiblePathX.get(0);
+			possiblePathX.remove(0); //removes path from list so we don't use it twice.
+		//}
+		//else {
+			//System.out.println("Error");
+			//System.exit(0);
+		//}
+		return x;
+	}
+	public int getPossiblePathY(){
+		int y = -1;
+		if(possiblePathY.size() > 0) {
+			y = possiblePathY.get(0);
+			possiblePathY.remove(0);
+		}
+		else {
+			System.out.println("Error");
+			System.exit(0);
+		}
+		return y;	
+	}
+	public boolean hasAlternatePath() {
+		return alternatePath;
+	}
+	public void setAlternatePath(boolean flag) {
+		alternatePath = flag;
 	}
 	public Node getRightChild() {
 		return right;
