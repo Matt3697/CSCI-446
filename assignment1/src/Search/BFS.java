b97bc08c4dca23e3a658bec9c9592aad9d8e7e0e
Carie Pointer <cariepointer@yahoo.com>
2018-10-08 14:20:52 -0600

    Add expanded node cost to BFS

--- assignment1/src/Search/BFS.java
+++ assignment1/src/Search/BFS.java
@@ -1,6 +1,7 @@
 package Search;
 
 import java.util.Queue;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /*
@@ -16,32 +17,39 @@ public class BFS {
 	}
 	
 	public void solve(Maze m) {
+		ArrayList<Node> expandedNodes = new ArrayList<>();
 		m.printMaze();
 		Node currNode = m.getStartingPoint();
 		nodeMaze = m.getNodeMatrix(); // Get maze as a 2D array of Nodes
 		System.out.println("start: (" + currNode.getX() + ", " + currNode.getY() + ")");  
 		q.add(currNode); 
+		
 		while (!q.isEmpty()) {
 			currNode = q.remove();
 			if (currNode.getValue() == '*') { // End has been found
 				System.out.println("total cost: " + currNode.getCost());
+				System.out.println("expanded nodes: " + expandedNodes.size());
 				while (currNode.getValue() != 'P') { // Update visual path of least cost in the maze, represented with 'o' char
-					m.updateValue(currNode.getX(), currNode.getY(), 'o');
+					m.updateValue(currNode.getX(), currNode.getY(), '.');
 					currNode = currNode.getPrev();
 				}
-				m.updateValue(currNode.getX(), currNode.getY(), 'o'); 
+				m.updateValue(currNode.getX(), currNode.getY(), '.'); 
 				m.printMaze();
+				break;
 			}
+			
 			else {
 				for (Node n : currNode.getNeighbors()) {
 					if (!n.isVisited()) { // If neighbor has not been visited, add it to queue and update cost
 						n.setVisited();
 						n.setCost(currNode.getCost() + 1); // Cost is current node cost + 1 for the neighbor
 						n.setPrev(currNode); // Previous node of neighbor is current node
-						m.updateValue(n.getX(), n.getY(), '.'); // (optional) Update char maze to represent nodes that have been visited
+						//m.updateValue(n.getX(), n.getY(), '.'); // (optional) Update char maze to represent nodes that have been visited
 						q.add(n); 
 					}
 				}
+			if (!expandedNodes.contains(currNode))
+				expandedNodes.add(currNode);
 			}
 		}
 	}
