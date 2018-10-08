7b68a5287ed3e9380933a0debb05cd9e20791c48
Matthew Sagen <matthewsagen@matthews-mbp.home>
2018-10-08 13:28:05 -0600

    unique mazes for dfs

--- assignment1/src/Search/Main.java
+++ assignment1/src/Search/Main.java
@@ -19,11 +19,15 @@ public class Main {
 		Maze openMaze = new Maze("open_maze");
 		BFS bfs = new BFS();
 		bfs.solve(mediumMaze);
-		Maze mediumMaze1 = new Maze("medium_maze");
+		
+		Maze mediumMaze1 = new Maze("medium_maze");//mazes for Depth First Search.
+		Maze largeMaze1 = new Maze("large_maze");
+		Maze openMaze1 = new Maze("open_maze");
 		DFS depthFirstSearch = new DFS();
 		depthFirstSearch.solve(mediumMaze1);
-		depthFirstSearch.solve(largeMaze);
-		depthFirstSearch.solve(openMaze);
+		depthFirstSearch.solve(largeMaze1);
+		depthFirstSearch.solve(openMaze1);
+		
 		PrintWriter writer = new PrintWriter("Output.txt", "UTF-8"); //outputs to directory Assignment1
 		printResults(writer);
 		writer.close();
