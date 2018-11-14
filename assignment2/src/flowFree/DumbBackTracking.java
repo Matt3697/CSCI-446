9fa253ff0e91a560100994ae6ca1696d978e7414
Carie Pointer <carie@wolfssl.com>
2018-11-14 15:36:21 -0700

    Add comments

--- assignment2/src/flowFree/DumbBackTracking.java
+++ assignment2/src/flowFree/DumbBackTracking.java
@@ -1,12 +1,14 @@
 package flowFree;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /*
  * Authors: Carie Pointer, Hugh Jackovich, Matthew Sagen
  * Date:    11/13/18
  * Artificial Intelligence: Assignment 2
+ * 
+ * Class DumbBackTracking search solves a flow-free maze
  */
-import java.util.ArrayList;
-import java.util.HashSet;
-
 
 public class DumbBackTracking {
 	Maze maze;
@@ -28,6 +30,7 @@ public class DumbBackTracking {
 		this.nodeMaze = maze.getNodeMatrix();
 	}
 	
+	/* Initializes recursive backtracking solver */
 	public void backtrack() {
 		colors = new HashSet<Character>();
 		startNodes = maze.getStartNodes();
@@ -42,29 +45,36 @@ public class DumbBackTracking {
 			System.out.println("no solution");
 	}
 	
+	
+	/* Recursive function to search through maze and assign colors to variables.
+	 * Returns true if all variables are colored and all constraints are held
+	 */
 	public boolean backtrackRecursive(Node[][] assignment) {
 		if (assignmentComplete(assignment)) {
-			System.out.println("Maze:");
-			maze.printNodeMatrix();
-			System.out.println();
+			System.out.println("Solution:");
+			printAssignment(assignment);
 			return true;
 		}
 		
+		// Get variable that has not yet been assigned 
 		Node n = getVariable();
 		if (n == null) {
 			return false;
 		}
-		System.out.println(colors);
-		printAssignment(assignment);
-		
+
+		// Start with first color in list of possible colors
 		for (char c : colors) {
 			
 			n.setValue(c);
+			
+			// Check if this color assignment is valid
 			if (canMakeAssignment(n, c, assignment)) {
 				
+				// Recursive call to function if previous assignment is valid
 				if (backtrackRecursive(assignment))
 					return true;
 				
+				// If recursive call fails, change assignment back to a blank
 				n.setValue('_');
 			}
 			else
@@ -74,6 +84,7 @@ public class DumbBackTracking {
 		return false;
 	}
 	
+	/* Returns first variable in list that is not assigned a color */
 	public Node getVariable() {
 		for (Node n : varList) {
 			if (n.getValue() == '_')
@@ -82,9 +93,10 @@ public class DumbBackTracking {
 		return null;
 	}
 	
+	/* Checks if current node has a valid assignment by checking each of its neighbors. 
+	 * Returns false if one of the neighbors invalidates a constraint, or true on success 
+	 */
 	public boolean canMakeAssignment(Node n, char c, Node[][] assignment) {
-		//System.out.println("checking neighbors of node " + n.getX() + " " + n.getY());
-		//System.out.println("current color: " + n.getValue());
 		for (Node neighbor : n.getNeighbors()) {
 			if(!checkNeighborConstraints(neighbor, c, assignment))
 				return false;
@@ -93,6 +105,9 @@ public class DumbBackTracking {
 		return true;
 	}
 	
+	/* Checks all neighbors of a given node n to ensure no constraints are broken 
+	 * Returns true on success, or false if one of the constraints is broken
+	 */
 	public boolean checkNeighborConstraints(Node n, char c, Node[][] assignment) {
 		int sameColor = 0;
 		int blanks = 0;
@@ -100,9 +115,8 @@ public class DumbBackTracking {
 		if (n.getValue() == '_')
 			return true;
 
-		//System.out.println("curr node " + n.getX() + " " + n.getY() + " color "+n.getValue());
+		// Count number of neighbors that are unassigned or the same color as n
 		for (Node neighbor : n.getNeighbors()) {
-			//System.out.println("neighbor node " + neighbor.getX() + " " + neighbor.getY() + " " + neighbor.getValue());
 			if (neighbor.getValue() == '_')
 				blanks++;
 			else if (neighbor.getValue() == n.getValue()) {
@@ -110,19 +124,24 @@ public class DumbBackTracking {
 			}
 			
 		}
+		
+		// Source cannot have more than one neighbor of the same color 
+		// and non-source nodes cannot have more than two neighbors of the same color
 		if ((sameColor > 1 && n.isSource()) || (sameColor > 2 && !n.isSource())) {
-			//System.out.println("failed");
 			return false;
 		}
 			
+		// Source nodes must have one neighbor of the same color
+		// and non-source nodes must have two neighbors of same color.
+		// Unassigned nodes are potential neighbors of same color.
 		if (((sameColor + blanks < 1) && n.isSource()) || ((sameColor + blanks < 2) && !n.isSource())) {
-			//System.out.println("sameColor + blanks = " + (sameColor + blanks));
 			return false;
 		}
 		
 		return true;
 	}
 	
+	/* Print method for the assignment */
 	public void printAssignment(Node[][] assignment) {
 		for (int i = 0; i < assignment.length; i++) {
 			for (int j = 0; j < assignment[0].length; j++) {
@@ -130,10 +149,12 @@ public class DumbBackTracking {
 			}
 			System.out.println();
 		}
-		System.out.println();
-		
+		System.out.println();	
 	}
 	
+	/* Checks if the assignment is completed by counting all of the assigned nodes.
+	 * returns true if all nodes have been assigned
+	 */
 	public boolean assignmentComplete(Node[][] assignment) {
 		int count = 0;
 		for (int i = 0; i < assignment.length; i++) {
