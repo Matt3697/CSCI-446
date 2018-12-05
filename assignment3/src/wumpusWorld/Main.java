5abaa2ae50fe392d56086e19468e4c20feb8ecdd
Hugh Jackovich <hughman25@gmail.com>
2018-12-04 19:20:28 -0700

    fixed Main.java to run all mazes

--- assignment3/src/wumpusWorld/Main.java
+++ assignment3/src/wumpusWorld/Main.java
@@ -14,19 +14,37 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		PrintWriter writer = new PrintWriter("Output.txt", "UTF-8"); //outputs to directory Assignment3
 		Agent agent = new Agent(0,0);//the agent always starts at square [0,0]
+		Agent agent1 = new Agent(0,0);
+		Agent agent2 = new Agent(0,0);
+		Agent agent3 = new Agent(0,0);
+		
 		Wumpus w = new Wumpus(4);
-		Maze maze1 = new Maze(5, 5, agent, w);
-		//Maze maze2 = new Maze(5,5, agent);
-		WumpusSolver wumpus = new WumpusSolver(maze1, agent, w, writer);
+		Wumpus w1 = new Wumpus(5);
+		Wumpus w2 = new Wumpus(8);
+		Wumpus w3 = new Wumpus(10);
+		
+		Maze maze = new Maze(4, 4, agent, w);
+		Maze maze1 = new Maze(5, 5, agent1, w1);
+		Maze maze2 = new Maze(8,8, agent2, w2);
+		Maze maze3 = new Maze(10,10, agent3, w3);
+		
+		WumpusSolver wumpus = new WumpusSolver(maze, agent, w, writer);
 		wumpus.solveMaze();
-		//Maze maze2 = new Maze(5,5, agent);
-		//Maze maze3 = new Maze(8,8);
-		//Maze maze4 = new Maze(10,10);
+
+		WumpusSolver wumpus1 = new WumpusSolver(maze1, agent1, w1, writer);
+		wumpus1.solveMaze();
+
+		WumpusSolver wumpus2 = new WumpusSolver(maze2, agent2, w2, writer);
+		wumpus2.solveMaze();
+
+		WumpusSolver wumpus3 = new WumpusSolver(maze3, agent3, w3, writer);
+		wumpus3.solveMaze();
+		
+		maze.printNodeMatrix(writer);
 		maze1.printNodeMatrix(writer);
-		//maze1.printNodeMatrix();
-		//maze2.printNodeMatrix();
-		//maze3.printNodeMatrix();
-		//maze4.printNodeMatrix();
+		maze2.printNodeMatrix(writer);
+		maze3.printNodeMatrix(writer);
+		
 		writer.close();
 		
 	}
